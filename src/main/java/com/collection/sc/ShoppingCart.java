d09e05434ece9047439da7e4995c06f1a9e92482
Sarawanan <sarawanan.mahalingam@icloud.com>
2022-04-03 07:43:24 +0530

    Refine the whole project

--- src/main/java/com/collection/sc/ShoppingCart.java
+++ src/main/java/com/collection/sc/ShoppingCart.java
@@ -3,26 +3,38 @@ package com.collection.sc;
 import java.util.Scanner;
 
 public class ShoppingCart {
-    static Products products;
-    static Cart cart;
-    static final Scanner scanner = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        products = new Products();
-        cart = new Cart();
+        new ShoppingCart().menu();
+    }
+
+    private int getInput() {
+        if (sc.hasNextInt()) {
+            return sc.nextInt();
+        } else {
+            System.out.println("Invalid input");
+            sc.next();
+            return -1;
+        }
+    }
+
+    public void menu() {
+        Products products = new Products();
+        Cart cart = new Cart();
 
         do {
             try {
                 System.out.println("MENU: [1: Products] [2: Add to Cart] [3: Remove from Cart] [4: Display Cart] [0: Exit]");
-                int input = scanner.nextInt();
+                int input = getInput();
 
                 switch (input) {
                     case 1 -> products.display();
                     case 2 -> {
                         System.out.print("Enter product id to add");
-                        Product product = products.getProductById(scanner.nextInt());
+                        Product product = products.getProductById(getInput());
                         System.out.print("Quantity");
-                        int qty = scanner.nextInt();
+                        int qty = getInput();
                         if (qty < product.getStock()) {
                             cart.addProductToCart(product, qty);
                             System.out.println("Product successfully added to cart");
@@ -32,14 +44,13 @@ public class ShoppingCart {
                     }
                     case 3 -> {
                         System.out.print("Enter product id to remove");
-                        cart.removeProductFromCart(products.getProductById(scanner.nextInt()));
+                        cart.removeProductFromCart(products.getProductById(getInput()));
                     }
                     case 4 -> cart.display();
                     case 0 -> System.exit(0);
                 }
             } catch (Exception exception) {
                 System.out.println(exception.getMessage());
-                scanner.next();
             }
         } while (true);
     }
